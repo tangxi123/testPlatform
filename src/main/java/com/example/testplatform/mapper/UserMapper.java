ea064ee6e67ec435a1db47564954894a0a291e41
Tang xi <tangxi669@163.com>
2018-10-12 17:46:10 +0800

    1.在UserMapper里新增了一个判断是否存在相同username的方法：ExistsByUsername 2.在AuthService里修改了是否存在相同用户名的判断，使用UserMapper的ExistsByUsername

--- src/main/java/com/example/testplatform/mapper/UserMapper.java
+++ src/main/java/com/example/testplatform/mapper/UserMapper.java
@@ -2,10 +2,14 @@ package com.example.testplatform.mapper;
 
 import com.example.testplatform.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
-@Repository
 @Mapper
 public interface UserMapper {
     int insertUser(User user);
+
+    @Select("SELECT EXISTS(SELECT 1 FROM users WHERE username = #{username})")
+    Boolean existsByUsername(@Param("username") String username);
 }
